c48b2bd8ece8e5aadc3012303088dbebc6d4a55c
blackwiz <wiz.stalker@gmail.com>
2016-09-27 11:21:39 +0700

    fix heroku

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,35 +4,22 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '6J790_lHUG25jfLSUXccZaCKskqds9ta',
+            'cookieValidationKey' => 'uH8W42d4KR-lVtqDW1l5rIyhZxhT64Et',
         ],
-        /*'view' => [
-			'theme' => [
-				'pathMap' => [
-					'@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
-				],
-			 ],
-		],*/
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=ec2-54-221-244-62.compute-1.amazonaws.com;dbname=d5ia2sbqqgdrgo', 
+            'username' => 'eqpzrgalxrfpdq',
+            'password' => 'RM381ZBeCzJnpLeo5N0qBU5M9S',
+            'charset' => 'UTF8',
+            'schemaMap' => [
+              'pgsql'=> [
+                'class'=>'yii\db\pgsql\Schema',
+                'defaultSchema' => 'public' //specify your schema here
+              ]
+            ], // PostgreSQL
+        ],
     ],
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        'generators' => [
-                //'crud' => ['class' => 'dee\gii\generators\crud\Generator'],
-                //'angular' => ['class' => 'dee\gii\generators\angular\Generator'],
-                //'mvc' => ['class' => 'dee\gii\generators\mvc\Generator'],
-                'migration' => ['class' => 'dee\gii\generators\migration\Generator'],
-            ],
-    ];
-}
 
 return $config;
--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,21 +4,22 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '7Cc6Y9oYhjtOvaAvRt_fH_F_SYmrtj_w',
+            'cookieValidationKey' => '2qILZu7i2C2Y1gXFlNina38qfdUyRooC',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=ec2-54-221-244-62.compute-1.amazonaws.com;dbname=d5ia2sbqqgdrgo', 
+            'username' => 'eqpzrgalxrfpdq',
+            'password' => 'RM381ZBeCzJnpLeo5N0qBU5M9S',
+            'charset' => 'UTF8',
+            'schemaMap' => [
+              'pgsql'=> [
+                'class'=>'yii\db\pgsql\Schema',
+                'defaultSchema' => 'public' //specify your schema here
+              ]
+            ], // PostgreSQL
         ],
     ],
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
 
 return $config;
