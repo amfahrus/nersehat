66ea58e3f919e9b5442b7503be109d692be25e7f
AmFahrus <f4ztr1k@yahoo.co.id>
2016-10-05 08:51:54 +0700

    Update left.php

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -5,7 +5,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?php echo $directoryAsset; ?>/img/user.png" class="img-circle" alt="User Image"/>
+                <img src="<?php echo $directoryAsset; ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
                 <p><?php echo Yii::$app->user->identity->username; ?></p>
