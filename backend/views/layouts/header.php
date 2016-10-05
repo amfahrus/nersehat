14649a88128a1f4d7ebe9355387ddf3fa97ce8a2
AmFahrus <f4ztr1k@yahoo.co.id>
2016-10-05 08:51:17 +0700

    Update header.php

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -26,13 +26,13 @@ use yii\helpers\Html;
 
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="<?php echo $directoryAsset; ?>/img/user.png" class="user-image" alt="User Image"/>
+                        <img src="<?php echo $directoryAsset; ?>/img/user2-160x160.jpg" class="user-image" alt="User Image"/>
                         <span class="hidden-xs"><?php echo Yii::$app->user->identity->username; ?></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
                         <li class="user-header">
-                            <img src="<?= $directoryAsset ?>/img/user.png" class="img-circle"
+                            <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle"
                                  alt="User Image"/>
 
                             <p>
